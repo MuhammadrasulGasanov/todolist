17938c2100eb7378ef7a2a79dfb4a44558dc5579
Muhammadrasul <magomedr.g@gmail.com>
2025-06-05 15:53:58 +0300

    use freezed in `AuthResponse`

--- lib/data/models/auth_response.dart
+++ lib/data/models/auth_response.dart
@@ -1,15 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'auth_response.g.dart';
+part 'auth_response.freezed.dart';
 
-@JsonSerializable()
-class AuthResponse {
-  AuthResponse({this.token, required this.userID, required this.userName});
-  final String? token;
-  final int userID;
-  final String userName;
+@freezed
+sealed class AuthResponse with _$AuthResponse {
+  factory AuthResponse({
+    String? token,
+    required String userID,
+    required String userName,
+  }) = _AuthResponse;
+
   factory AuthResponse.fromJson(Map<String, dynamic> json) =>
       _$AuthResponseFromJson(json);
-
-  Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
 }
