e810a66e73ef222ea6cdaf2c9c70f242b87341cd
Muhammadrasul <magomedr.g@gmail.com>
2025-06-05 15:47:41 +0300

    make token nullable

--- lib/data/models/auth_response.dart
+++ lib/data/models/auth_response.dart
@@ -4,15 +4,11 @@ part 'auth_response.g.dart';
 
 @JsonSerializable()
 class AuthResponse {
-  AuthResponse({
-    required this.token,
-    required this.userID,
-    required this.userName,
-  });
-  final String token;
+  AuthResponse({this.token, required this.userID, required this.userName});
+  final String? token;
   final int userID;
   final String userName;
-    factory AuthResponse.fromJson(Map<String, dynamic> json) =>
+  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
       _$AuthResponseFromJson(json);
 
   Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
